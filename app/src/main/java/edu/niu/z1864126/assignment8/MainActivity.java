0e43fb91b953b03a9730cb03dba5ac24013cc380
Brendon <brewerb1993@gmail.com>
2020-12-01 12:11:47 -0600

    -Added menu functionality for Add activity (Waiting on Delete activity to be finished before implementing menu functionality for delete) - Back button under the Add activity allows for return to the main activity - Any implementations requiring a dynamic interface or SQL are on the to do list - Next change will include the addition of a scrollview to the Add activity - Will be adding comments to everything shortly - Will also be placing a bunch of strings into resource files

--- app/src/main/java/edu/niu/z1864126/assignment8/MainActivity.java
+++ app/src/main/java/edu/niu/z1864126/assignment8/MainActivity.java
@@ -2,13 +2,40 @@ package edu.niu.z1864126.assignment8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private int requestcode = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_res, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem choice)
+    {
+        switch (choice.getItemId())
+        {
+            case R.id.Add:
+                Intent addActivitySwitch = new Intent(this, add_screen_activity.class);
+                startActivityForResult(addActivitySwitch, requestcode);
+            case R.id.Delete:
+            default:
+                return super.onOptionsItemSelected(choice);
+        }
     }
 }
--- app/src/main/java/edu/niu/z1864126/assignment8/add_screen_activity.java
+++ app/src/main/java/edu/niu/z1864126/assignment8/add_screen_activity.java
@@ -3,6 +3,7 @@ package edu.niu.z1864126.assignment8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class add_screen_activity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class add_screen_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_screen_activity);
+    }
+
+    public void backToMain(View v)
+    {
+        finish();
     }
 }
